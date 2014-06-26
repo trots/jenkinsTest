0ad6223af86e138c0a5d992fb7653ab13644f34b
dbolotsev <dbolotsev@inobitec.com>
2014-06-26 14:12:14 +0300

    С ошибкой

--- jenkinsTest/main.cpp
+++ jenkinsTest/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
 
-  cout << "Hello, world" << edghghgdfhherl;
+  cout << "Hello, world" << edghghgdgfgfhherl;
   cout << "Hello, Jenkins" << endl;
 
   QFile file("TestFile.txt");
